d98638db549ee5618044076570902c6761658161
Simon Elliott <simoncelliott@gmail.com>
2020-04-14 20:59:35 +0100

    changed variable declarations

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "google_compute_firewall" "ingress" {
-  name    = "${var.env}-${var.name}-ingress"
-  network = "${var.network_attachment}"
-  direction = "INGRESS"
-    source_ranges = ["${var.ingress_ips}"]
+  name          = "${var.env}-${var.name}-ingress"
+  network       = var.network_attachment
+  direction     = "INGRESS"
+  source_ranges = var.ingress_ips
 
   allow {
     protocol = "icmp"
@@ -10,21 +10,19 @@ resource "google_compute_firewall" "ingress" {
 
   allow {
     protocol = "tcp"
-    ports    = ["${var.ports}"]
+    ports    = var.ports
   }
 
-  source_tags = ["${var.source_tags}"]
-}  
+  source_tags = var.source_tags
+}
 
 resource "google_compute_firewall" "egress" {
-  name    = "${var.env}-${var.name}-egress"
-  network = "${var.network_attachment}"
-  direction = "EGRESS"
-    destination_ranges = ["${var.egress_ips}"]
-
+  name               = "${var.env}-${var.name}-egress"
+  network            = var.network_attachment
+  direction          = "EGRESS"
+  destination_ranges = var.egress_ips
 
   allow {
     protocol = "all"
   }
-
-}  
+}
