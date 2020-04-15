0393f72ae908f49d936782f076f4a78ea773de8e
Simon Elliott <simoncelliott@gmail.com>
2020-04-15 18:08:36 +0100

    TF12 cleanup

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
 resource "google_compute_firewall" "ingress" {
-  name          = "${var.env}-${var.name}-ingress"
-  network       = var.network_attachment
-  direction     = "INGRESS"
-  source_ranges = var.ingress_ips
+  name                              = "${var.env}-${var.name}-ingress"
+  network                           = var.network_attachment
+  direction                         = "INGRESS"
+  source_ranges                     = var.ingress_ips
 
   allow {
-    protocol = "icmp"
+    protocol                        = "icmp"
   }
 
   allow {
-    protocol = "tcp"
-    ports    = var.ports
+    protocol                        = "tcp"
+    ports                           = var.ports
   }
 
-  source_tags = var.source_tags
+  source_tags                       = var.source_tags
 }
 
 resource "google_compute_firewall" "egress" {
-  name               = "${var.env}-${var.name}-egress"
-  network            = var.network_attachment
-  direction          = "EGRESS"
-  destination_ranges = var.egress_ips
+  name                              = "${var.env}-${var.name}-egress"
+  network                           = var.network_attachment
+  direction                         = "EGRESS"
+  destination_ranges                = var.egress_ips
 
   allow {
-    protocol = "all"
+    protocol                        = "all"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable env                        {}
 variable name                       {}
 variable network_attachment         {}
-variable ingress_ips                {type="list"}
-variable egress_ips                 {type="list"}
-variable ports                      {type="list"}
-variable source_tags                {type="list"}
+variable ingress_ips                {type = list(string)}
+variable egress_ips                 {type = list(string)}
+variable ports                      {type = list(string)}
+variable source_tags                {type = list(string)}
